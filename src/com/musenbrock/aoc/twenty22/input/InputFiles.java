7d40cb26a58f79810941a6031f1eb89aa246f241
Michael Musenbrock <michael.musenbrock@gmail.com>
2022-12-04 19:24:33 +0100

    Day 02: Add input files

--- src/com/musenbrock/aoc/twenty22/input/InputFiles.java
+++ src/com/musenbrock/aoc/twenty22/input/InputFiles.java
@@ -3,7 +3,8 @@ package com.musenbrock.aoc.twenty22.input;
 import java.nio.file.Path;
 
 public enum InputFiles {
-  DAY_01("input_day01", "input_day01_example");
+  DAY_01("input_day01", "input_day01_example"),
+  DAY_02("input_day02_rps_strategy_guide", "input_day02_rps_strategy_guide_example");
 
   public static Path FILE_PATH = Path.of("/home/michael/work/advent_of_code_2022/resources");
 
