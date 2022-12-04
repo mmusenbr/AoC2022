cb4b56b1266d1f020b6f70a8215853af03a6546b
Michael Musenbrock <michael.musenbrock@gmail.com>
2022-12-04 20:55:43 +0100

    Day02: RockPaperScissors: Implement Day02 calculation methods

--- src/com/musenbrock/aoc/twenty22/Day02.java
+++ src/com/musenbrock/aoc/twenty22/Day02.java
@@ -1,11 +1,29 @@
 package com.musenbrock.aoc.twenty22;
 
+import com.musenbrock.aoc.twenty22.input.RockPaperScissorsStrategyInputReader;
+import com.musenbrock.aoc.twenty22.rockpaperscissors.PointsCalculator;
+import com.musenbrock.aoc.twenty22.rockpaperscissors.RockPaperScissorsStrategy;
+import java.io.IOException;
+import java.util.List;
+
 public class Day02 {
+  final RockPaperScissorsStrategyInputReader inputReader = new RockPaperScissorsStrategyInputReader();
+  final PointsCalculator pointsCalculator = new PointsCalculator();
+  final List<RockPaperScissorsStrategy> gameAndStrategies;
+  final List<Integer> gameScore;
+
+  public Day02() throws IOException {
+    gameAndStrategies = inputReader.readRockPaperScissorsStrategies();
+    gameScore = gameAndStrategies.stream()
+                                 .map(strategy -> pointsCalculator.fromGame(strategy.opponent(), strategy.own()))
+                                 .toList();
+  }
+
   public int getScoreOfGame(int gameIndex) {
-    return -1;
+    return gameScore.get(gameIndex);
   }
 
   public int getOverallScore() {
-    return -1;
+    return gameScore.stream().mapToInt(i -> i).sum();
   }
 }
--- src/com/musenbrock/aoc/twenty22/input/RockPaperScissorsStrategyInputReader.java
+++ src/com/musenbrock/aoc/twenty22/input/RockPaperScissorsStrategyInputReader.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.List;
 
 public class RockPaperScissorsStrategyInputReader {
-  public static List<RockPaperScissorsStrategy> readRockPaperScissorsStrategies() throws IOException {
+  public List<RockPaperScissorsStrategy> readRockPaperScissorsStrategies() throws IOException {
     return FileReader.readInputFileSkipEmptyLines(InputFiles.DAY_02)
                      .map(RockPaperScissorsStrategyMapper::fromString)
                      .toList();
