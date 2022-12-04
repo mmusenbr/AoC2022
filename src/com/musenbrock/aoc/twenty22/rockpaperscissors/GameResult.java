a72eceae7057b6625f7dc4a7c1d1f3b8bd141c84
Michael Musenbrock <michael.musenbrock@gmail.com>
2022-12-04 20:33:24 +0100

    RockPaperScissors: Add basic datatypes: GameResult, RockPaperScissors

---
+++ src/com/musenbrock/aoc/twenty22/rockpaperscissors/GameResult.java
@@ -0,0 +1,5 @@
+package com.musenbrock.aoc.twenty22.rockpaperscissors;
+
+public enum GameResult {
+  WIN, DRAW, LOSS
+}
---
+++ src/com/musenbrock/aoc/twenty22/rockpaperscissors/RockPaperScissors.java
@@ -0,0 +1,5 @@
+package com.musenbrock.aoc.twenty22.rockpaperscissors;
+
+public enum RockPaperScissors {
+  ROCK, PAPER, SCISSORS
+}
